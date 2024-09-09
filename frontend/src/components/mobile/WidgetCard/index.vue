d44f69f6028e39e1dfc29fd3af9529588ef4e472
monoland <mono.socialmedia@gmail.com>
2024-09-09 11:36:01 +0700

    update

--- frontend/src/components/mobile/WidgetCard/index.vue
+++ frontend/src/components/mobile/WidgetCard/index.vue
@@ -1,11 +1,34 @@
 <template>
-    <v-sheet
-        :class="vertical ? 'flex-column' : null"
-        color="grey-lighten-5"
-        class="d-flex align-center justify-center pa-3 border-thin"
-        rounded
-    >
-        <slot></slot>
+    <v-sheet color="grey-lighten-5" class="border-thin" rounded>
+        <v-toolbar density="compact">
+            <v-toolbar-title class="text-caption text-uppercase">{{
+                title
+            }}</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn density="compact" icon @click="showTable = !showTable">
+                <v-icon size="small">table_chart</v-icon>
+            </v-btn>
+        </v-toolbar>
+
+        <div class="position-relative">
+            <slot></slot>
+
+            <v-expand-transition>
+                <div
+                    v-if="showTable"
+                    class="transition-fast-in-fast-out bg-white darken-2 v-card--reveal white--text"
+                    style="height: 100%"
+                >
+                    <v-sheet
+                        class="position-relative h-100 overflow-y-auto overflow-x-hidden"
+                    >
+                        <slot name="table"></slot>
+                    </v-sheet>
+                </div>
+            </v-expand-transition>
+        </div>
     </v-sheet>
 </template>
 
@@ -14,7 +37,12 @@ export default {
     name: "widget-card",
 
     props: {
+        title: String,
         vertical: Boolean,
     },
+
+    data: () => ({
+        showTable: false,
+    }),
 };
 </script>
