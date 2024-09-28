966cacc4c53351af66db1c56725a0f470964382b
monoland <mono.socialmedia@gmail.com>
2024-09-28 13:17:40 +0700

    update

--- frontend/src/components/desktop/PageHome/index.vue
+++ frontend/src/components/desktop/PageHome/index.vue
@@ -1,82 +1,72 @@
 <template>
-	<v-app-bar
-		:color="`${theme}-lighten-5`"
-		height="72"
-		scroll-behavior="hide elevate"
-		scroll-threshold="87"
-	>
-		<v-app-bar-nav-icon @click="railMode = !railMode"></v-app-bar-nav-icon>
+    <v-app-bar
+        :color="`${theme}-lighten-5`"
+        height="72"
+        scroll-behavior="hide elevate"
+        scroll-threshold="87"
+    >
+        <v-app-bar-nav-icon @click="railMode = !railMode"></v-app-bar-nav-icon>
 
-		<v-toolbar-title class="text-body-2 font-weight-bold text-uppercase">{{
-			module.name
-		}}</v-toolbar-title>
+        <v-toolbar-title class="text-body-2 font-weight-bold text-uppercase">{{
+            module.name
+        }}</v-toolbar-title>
 
-		<v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-		<slot name="toolbar"></slot>
+        <slot name="toolbar" :statuses="statuses"></slot>
 
-		<v-btn
-			icon
-			@click="gotoAccountDashboard"
-		>
-			<v-icon>exit_to_app</v-icon>
-		</v-btn>
-	</v-app-bar>
+        <v-btn icon @click="gotoAccountModule">
+            <v-icon>exit_to_app</v-icon>
+        </v-btn>
+    </v-app-bar>
 
-	<v-main style="min-height: 100dvh">
-		<v-container>
-			<page-paper max-width="100%">
-				<div class="d-flex justify-center mt-3">
-					<v-chip
-						:color="theme"
-						class="text-white"
-						density="comfortable"
-						size="small"
-						variant="flat"
-					>
-						Module Pages
-					</v-chip>
-				</div>
+    <v-main style="min-height: 100dvh">
+        <v-container>
+            <page-paper max-width="100%">
+                <div class="d-flex justify-center mt-3">
+                    <v-chip
+                        :color="theme"
+                        class="text-white"
+                        density="comfortable"
+                        size="small"
+                        variant="flat"
+                    >
+                        Module Pages
+                    </v-chip>
+                </div>
 
-				<v-sheet
-					class="mx-auto bg-transparent pa-4"
-					:style="
-						$vuetify.display.width >= 1280
-							? { maxWidth: '1164px' }
-							: { maxWidth: '640px' }
-					"
-				>
-					<v-row
-						justify="center"
-						dense
-					>
-						<v-col
-							v-for="(page, index) in dockMenus"
-							class="d-flex justify-center"
-							cols="1"
-							sm="2"
-							:key="index"
-						>
-							<widget-apps
-								:color="`${highlight}-darken-1`"
-								:highlight="`white`"
-								:icon="page.icon"
-								:label="page.name"
-								rounded="xl"
-								@click="openPage(page)"
-							></widget-apps>
-						</v-col>
-					</v-row>
-				</v-sheet>
-			</page-paper>
+                <v-sheet
+                    class="mx-auto bg-transparent pa-4"
+                    :style="
+                        $vuetify.display.width >= 1280
+                            ? { maxWidth: '1164px' }
+                            : { maxWidth: '640px' }
+                    "
+                >
+                    <v-row justify="center" dense>
+                        <v-col
+                            v-for="(page, index) in dockMenus"
+                            class="d-flex justify-center"
+                            cols="1"
+                            sm="2"
+                            :key="index"
+                        >
+                            <widget-apps
+                                :color="`${highlight}-darken-1`"
+                                :highlight="`white`"
+                                :icon="page.icon"
+                                :label="page.name"
+                                rounded="xl"
+                                @click="openPage(page)"
+                            ></widget-apps>
+                        </v-col>
+                    </v-row>
+                </v-sheet>
+            </page-paper>
 
-			<slot
-				:record="record"
-				:store="store"
-				:theme="theme"
-			></slot>
-		</v-container>
-	</v-main>
+            <slot :record="record" :store="store" :theme="theme"></slot>
+        </v-container>
+    </v-main>
 </template>
 
 <script>
@@ -84,77 +74,84 @@ import { usePageStore } from "@pinia/pageStore";
 import { storeToRefs } from "pinia";
 
 export default {
-	name: "page-home",
+    name: "page-home",
 
-	emits: {
-		initialized: null,
-	},
+    emits: {
+        initialized: null,
+    },
 
-	props: {
-		maxWidth: {
-			type: String,
-			default: "900px",
-		},
+    props: {
+        maxWidth: {
+            type: String,
+            default: "900px",
+        },
 
-		pageName: {
-			type: String,
-			default: null,
-		},
+        pageName: {
+            type: String,
+            default: null,
+        },
 
-		pageKey: {
-			type: String,
-			default: null,
-		},
-	},
+        pageKey: {
+            type: String,
+            default: null,
+        },
+    },
 
-	setup(props) {
-		const store = usePageStore();
+    setup(props) {
+        const store = usePageStore();
 
-		store.pageName = props.pageName;
-		store.pageKey = props.pageKey;
+        store.pageName = props.pageName;
+        store.pageKey = props.pageKey;
 
-		const {
-			auth,
-			dockMenus,
-			highlight,
-			module,
-			page,
-			railMode,
-			record,
-			theme,
-		} = storeToRefs(store);
+        const {
+            auth,
+            dockMenus,
+            highlight,
+            module,
+            page,
+            railMode,
+            record,
+            statuses,
+            theme,
+        } = storeToRefs(store);
 
-		const { getDashboard } = store;
+        const { clearFilters, getDashboard } = store;
 
-		return {
-			auth,
-			dockMenus,
-			highlight,
-			module,
-			page,
-			railMode,
-			record,
-			theme,
+        return {
+            auth,
+            dockMenus,
+            highlight,
+            module,
+            page,
+            railMode,
+            record,
+            statuses,
+            theme,
 
-			getDashboard,
-			store,
-		};
-	},
+            clearFilters,
+            getDashboard,
+            store,
+        };
+    },
 
-	mounted() {
-		this.getDashboard((response) => {
-			this.$emit("initialized", { record: response });
-		});
-	},
+    mounted() {
+        this.getDashboard((response) => {
+            this.$emit("initialized", { record: response });
+        });
+    },
 
-	methods: {
-		gotoAccountDashboard: function () {
-			this.$router.push({ name: "account-dashboard" });
-		},
+    methods: {
+        gotoAccountModule: function () {
+            this.clearFilters();
 
-		openPage: function (page) {
-			this.$router.push({ name: page.slug });
-		},
-	},
+            this.$router.push({ name: "account-module" });
+        },
+
+        openPage: function (page) {
+            this.clearFilters();
+
+            this.$router.push({ name: page.slug });
+        },
+    },
 };
 </script>
